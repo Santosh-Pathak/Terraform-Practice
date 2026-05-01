ff64896a9bb81fc8b5d148aa68b8e2781c1f69b3
Santosh Pathak <santosh.pathak@tedekstra.com>
2026-05-01 07:43:14 +0530

    terraform Basic

---
+++ main.tf
@@ -0,0 +1,15 @@
+# comment
+
+# simple file automation with code
+
+#block parameter {
+#
+#	arguments
+#
+#}
+
+resource local_file my_file {
+#arguments
+	filename = "demo.txt"
+	content = "Hello Dosto"
+}
